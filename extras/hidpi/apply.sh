afc04d5d2be73b3019b922e3c99ec9ff4bcf60c2
pexcn <pexcn97@gmail.com>
2024-08-24 12:37:50 +0800

    fix enable hidpi script

--- extras/hidpi/apply.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-curl https://github.com/pexcn/hackintosh-fujitsu-lifebook-u938/raw/master/extras/hidpi/DisplayVendorID-4d10/DisplayProductID-1483 \
--L -O --create-dirs --output-dir /Library/Displays/Contents/Resources/Overrides/DisplayVendorID-4d10/
-
-chown -R root:wheel /Library/Displays/Contents/Resources/Overrides
---
+++ extras/hidpi/enable.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+VID=4d10
+PID=1483
+
+curl https://github.com/pexcn/hackintosh-fujitsu-lifebook-u938/raw/master/extras/hidpi/DisplayVendorID-${VID}/DisplayProductID-${PID} \
+  -L -O --create-dirs --output-dir /Library/Displays/Contents/Resources/Overrides/DisplayVendorID-${VID}/
+
+chown -R root:wheel /Library/Displays/Contents/Resources/Overrides
+chmod -R 0755 /Library/Displays/Contents/Resources/Overrides
+chmod 0644 /Library/Displays/Contents/Resources/Overrides/DisplayVendorID-${VID}/DisplayProductID-${PID}
