d05a90f9028f279940fa494f5973edb8f5789cda
pexcn <pexcn97@gmail.com>
2024-09-08 01:17:04 +0800

    fix hidpi script not work #7

--- extras/hidpi/enable.sh
+++ extras/hidpi/enable.sh
@@ -3,11 +3,8 @@
 VID=4d10
 PID=1483
 
+mkdir -p /Library/Displays/Contents/Resources/Overrides/DisplayVendorID-${VID}/
 curl https://github.com/pexcn/hackintosh-fujitsu-lifebook-u938/raw/master/extras/hidpi/DisplayVendorID-${VID}/DisplayProductID-${PID} \
-  -L -O --create-dirs --output-dir /Library/Displays/Contents/Resources/Overrides/DisplayVendorID-${VID}/
-
-chown -R root:wheel /Library/Displays/Contents/Resources/Overrides
-chmod -R 0755 /Library/Displays/Contents/Resources/Overrides
-chmod 0644 /Library/Displays/Contents/Resources/Overrides/DisplayVendorID-${VID}/DisplayProductID-${PID}
+  -L -O --output-dir /Library/Displays/Contents/Resources/Overrides/DisplayVendorID-${VID}/
 
 defaults write /Library/Preferences/com.apple.windowserver DisplayResolutionEnabled -bool true
--- extras/voltageshift/enable.sh
+++ extras/voltageshift/enable.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
+mkdir -p /usr/local/bin/
 curl https://github.com/pexcn/hackintosh-fujitsu-lifebook-u938/raw/master/extras/voltageshift/voltageshift \
-  -L -O --create-dirs --output-dir /usr/local/bin/
-chown root:wheel /usr/local/bin/voltageshift
+  -L -O --output-dir /usr/local/bin/
 chmod +x /usr/local/bin/voltageshift
 
+mkdir -p /Library/LaunchDaemons/
 curl https://github.com/pexcn/hackintosh-fujitsu-lifebook-u938/raw/master/extras/voltageshift/com.sicreative.VoltageShift.plist \
   -L -O --output-dir /Library/LaunchDaemons/
-chown root:wheel /Library/LaunchDaemons/com.sicreative.VoltageShift.plist
 
 launchctl bootstrap system /Library/LaunchDaemons/com.sicreative.VoltageShift.plist
