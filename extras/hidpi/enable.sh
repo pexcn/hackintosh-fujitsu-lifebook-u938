3ff6b597cff394cbb425f85d56ac52f5e9ae7f9a
pexcn <pexcn97@gmail.com>
2024-10-22 23:00:23 +0800

    Ported to macOS Catalina

--- extras/hidpi/enable.sh
+++ extras/hidpi/enable.sh
@@ -5,6 +5,6 @@ PID=1483
 
 mkdir -p /Library/Displays/Contents/Resources/Overrides/DisplayVendorID-${VID}/
 curl https://github.com/pexcn/hackintosh-fujitsu-lifebook-u938/raw/master/extras/hidpi/DisplayVendorID-${VID}/DisplayProductID-${PID} \
-  -L -O --output-dir /Library/Displays/Contents/Resources/Overrides/DisplayVendorID-${VID}/
+  -L -o /Library/Displays/Contents/Resources/Overrides/DisplayVendorID-${VID}/DisplayProductID-${PID}
 
 defaults write /Library/Preferences/com.apple.windowserver DisplayResolutionEnabled -bool true
--- extras/voltageshift/enable.sh
+++ extras/voltageshift/enable.sh
@@ -2,11 +2,11 @@
 
 mkdir -p /usr/local/bin/
 curl https://github.com/pexcn/hackintosh-fujitsu-lifebook-u938/raw/master/extras/voltageshift/voltageshift \
-  -L -O --output-dir /usr/local/bin/
+  -L -o /usr/local/bin/voltageshift
 chmod +x /usr/local/bin/voltageshift
 
 mkdir -p /Library/LaunchDaemons/
 curl https://github.com/pexcn/hackintosh-fujitsu-lifebook-u938/raw/master/extras/voltageshift/com.sicreative.VoltageShift.plist \
-  -L -O --output-dir /Library/LaunchDaemons/
+  -L -o /Library/LaunchDaemons/com.sicreative.VoltageShift.plist
 
 launchctl bootstrap system /Library/LaunchDaemons/com.sicreative.VoltageShift.plist
